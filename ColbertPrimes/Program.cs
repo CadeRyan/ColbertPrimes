71c561c5191b48944e08eb1d7ab7f7f5e5d0e79a
direwolf6 <ryanc45@tcd.ie>
2019-10-21 08:18:26 +0100

    adding the code for multi-threading

--- ColbertPrimes/Program.cs
+++ ColbertPrimes/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Numerics;
@@ -13,61 +14,88 @@ namespace ColbertPrimes
     {
         static void Main(string[] args)
         {
-            int n = 31100000; //update value for n as starting value
+            int startingPoint = 31000000;
+
+            Task[] taskArray = new Task[5];
+            for (int i = 0; i < taskArray.Length; i++)
+            {
+                taskArray[i] = Task.Factory.StartNew((Object obj) =>
+                {
+                    InputData data = obj as InputData;
+                    ProcessBatch(data.index, data.startingPoint);
+                }, new InputData(i, startingPoint));
+
+                startingPoint += 200;
+            }
+            Task.WaitAll(taskArray);
+
+            MergeFiles(GetFiles());
+
+
+            Console.WriteLine("done");
+            Console.ReadKey();
+        }
+
+        static string[] GetFiles()
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(@"C:\Users\cader\Desktop\primes\");
+            return dinfo.GetFiles("*.txt").Select(f => f.FullName).ToArray();
+        }
+
+        static void MergeFiles(string[] files)
+        {
+            using (StreamWriter sw = File.CreateText(@"C:\Users\cader\Desktop\primes\possiblePrimes.txt"))
+            {
+                foreach (var filename in files)
+                {
+                    Console.WriteLine(filename);
+                    using (StreamReader sr = File.OpenText(filename))
+                    {
+                        string s;
+                        while ((s = sr.ReadLine()) != null)
+                        {
+                            sw.WriteLine(s);
+                        }
+                    }
+                }
+            }
+        }
+
+        static void ProcessBatch(int i, int startingPoint)
+        {
+            int n = startingPoint;
             BigInteger twoN = BigInteger.Pow(2, n);
             Console.WriteLine("Starting...");
             double composite = 0;
             List<int> possiblePrimes = new List<int>();
 
-            for (int count = 0; count < 1000; count ++)
+            for (int count = 0; count < 200; count++)
             {
                 BigInteger proth = new BigInteger();
                 int k = 21181;
 
-                Stopwatch watch = new Stopwatch();
-
-                watch.Start();
                 proth = k * twoN + 1;
-                watch.Stop();
-                Console.WriteLine("Time to calculate proth number: " + watch.ElapsedMilliseconds);
 
-
-                watch.Restart();
                 if (IsPossiblyPrime(proth))
                 {
-                    Console.WriteLine("Possible Prime");
                     possiblePrimes.Add(n);
-                    //write number to a file
                 }
                 else
                 {
-                    Console.WriteLine("Not Prime");
                     composite++;
                 }
-                watch.Stop();
-                Console.WriteLine("Time to evalute possible primality: " + watch.ElapsedMilliseconds);
-
-                // do something with the list of n's
-
                 twoN *= 2;
                 n++;
             }
 
-            string path = @"C:\Users\cader\Desktop\possiblePrimes.txt";
+            string path = $@"C:\Users\cader\Desktop\primes\possiblePrimes{i}.txt";
             TextWriter tw = new StreamWriter(path);
 
             foreach (int s in possiblePrimes)
             {
-                Console.WriteLine(s);
                 tw.WriteLine(s);
             }
-
             tw.Close();
-
-            Console.WriteLine(n);
-
-            Console.WriteLine("Percentage eliminated: " + composite/1000);
-            Console.ReadKey();
         }
 
         static bool IsPossiblyPrime(BigInteger proth)
@@ -90,6 +118,17 @@ namespace ColbertPrimes
                 EnableSsl = true
             };
             client.Send("prothprimenotifier@gmail.com", "ryanc45@tcd.ie", "Found a Prime!", $"{k} * 2^{n} + 1 is Prime!");
+        }
+    }
+    class InputData
+    {
+        public int index;
+        public int startingPoint;
+
+        public InputData(int i, int _startingPoint)
+        {
+            index = i;
+            startingPoint = _startingPoint;
         }
     }
 }
